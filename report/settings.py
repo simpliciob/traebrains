8f49d655cf42ba0da1d631665ec69bced4fe6712
simpliciob <bachelorsithole@gmail.com>
2018-04-02 18:25:32 +0200

    take it ease

--- report/settings.py
+++ report/settings.py
@@ -142,12 +142,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Extra places for collectstatic to find static files.
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-    #s.path.join(PROJECT_ROOT, 'static'),
-#]
 STATICFILES_DIRS = [
-    BASE_DIR +  '/static/',
+    s.path.join(PROJECT_ROOT, 'static'),
 ]
+#STATICFILES_DIRS = [
+    #BASE_DIR +  '/static/',
+#]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
                   
